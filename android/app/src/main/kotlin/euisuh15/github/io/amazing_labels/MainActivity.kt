8c8cafb09ca1ae31a9fe128140cd5d5030c16ad0
EuiSuh (John) Jeong <55032583+euisuh15@users.noreply.github.com>
2022-07-13 16:21:43 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/euisuh15/github/io/amazing_labels/MainActivity.kt
@@ -0,0 +1,6 @@
+package euisuh15.github.io.amazing_labels
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AmazingLabels',
+      theme: ThemeData(
+          backgroundColor: Colors.blue[200],
+          fontFamily: 'Raleway',
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.blue[200],
+          )
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to Flutter'),
+      ),
+      body: const Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
